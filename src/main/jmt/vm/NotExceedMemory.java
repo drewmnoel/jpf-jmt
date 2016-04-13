a71fa25cb89bc235a6841f5dffa9665f52e352da
Drew Noel <drewmnoel@gmail.com>
2016-04-13 16:58:51 -0400

    Add a property for memory

---
+++ src/main/jmt/vm/NotExceedMemory.java
@@ -0,0 +1,39 @@
+package jmt.vm;
+
+import gov.nasa.jpf.Config;
+import gov.nasa.jpf.GenericProperty;
+import gov.nasa.jpf.search.Search;
+import gov.nasa.jpf.vm.VM;
+
+public class NotExceedMemory extends GenericProperty {
+  Long memory_limit;
+  boolean has_exceeded;
+
+  public NotExceedMemory (Config config) {
+    has_exceeded = false;
+    memory_limit = config.getLong("jmt.memory_limit", 0);
+  }
+
+  @Override
+  public String getExplanation () {
+    return "not exceed memory";
+  }
+
+  @Override
+  public String getErrorMessage () {
+    if (has_exceeded == true) {
+      return "Memory exceeded " + memory_limit;
+    }
+
+    return null;
+  }
+
+  @Override
+  public boolean check (Search search, VM vm) {
+    Runtime r = Runtime.getRuntime();
+    long heap_used = r.totalMemory() - r.freeMemory();
+    has_exceeded = heap_used > memory_limit;
+
+    return !has_exceeded;
+  }
+}
