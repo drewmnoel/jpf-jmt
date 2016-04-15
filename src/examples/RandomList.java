3f9e582925f04f4ddab018e6c085edb2df4b7d5f
Drew Noel <drewmnoel@gmail.com>
2016-04-15 15:43:33 -0400

    Document RandomList example

--- src/examples/RandomList.java
+++ src/examples/RandomList.java
@@ -1,7 +1,16 @@
 import java.util.LinkedList;
 import java.util.Random;
 
+/**
+ * A very bad random list generator. Could run forever and eat up
+ * all the memory. JPF will be used to catch it when it gets too big
+ */
 public class RandomList {
+  /**
+   * Entry point, entire program is here
+   *
+   * @param args[] Ignored
+   */
   public static void main(String args[]) {
     Random r = new Random();
     LinkedList<Integer> l = new LinkedList<>();
